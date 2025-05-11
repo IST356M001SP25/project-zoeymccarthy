759a667539738629d213d0d66157255419c4eaf6
zoeymccarthy <zpmccart@syr.edu>
2025-05-10 23:29:22 -0400

    updated dashboard with visualizations for price and volume, and made past visualizations more readable

--- code/analysis.py
+++ code/analysis.py
@@ -44,15 +44,48 @@ st.title("Stock Drop + Bond Yield Rise Condition Checker")
 st.write(f"Showing dates where stock return < 0 and bond yield change <= 0")
 
 #add filter by ticker
-selected_ticker = st.selectbox("Select a stock ticker", merged['ticker'].unique())
+selected_ticker = st.selectbox("Select a stock ticker to compare with bond yields:", merged['ticker'].unique())
 filtered = condition_met[condition_met['ticker'] == selected_ticker]
 
 # Show table
 st.dataframe(filtered[['date', 'ticker', 'return', 'bond_yield_change']])
 
-# Optional: Plot with seaborn
+#plot differences with seaborn
 fig, ax = plt.subplots()
 sns.scatterplot(data=filtered, x='date', y='return', hue='bond_yield_change', palette='coolwarm', ax=ax)
+plt.xticks(rotation=45) # Rotate labels by 45 degrees
+plt.tight_layout()
 ax.axhline(0, color='gray', linestyle='--')
 ax.set_title(f"Negative {selected_ticker} Returns with Non-negative Bond Yield Change")
 st.pyplot(fig)
+
+#plotting price and volume data
+st.title("Stock Price and Volume Over Time")
+#dropdown to select ticker
+selected_ticker = st.selectbox("Select a stock ticker:", stocks['ticker'].unique())
+
+#filter data for the selected ticker
+selected_data = stocks[stocks['ticker'] == selected_ticker]
+
+#price over time
+st.subheader(f"Price Over Time for {selected_ticker}")
+fig1, ax1 = plt.subplots(figsize=(10, 6))
+sns.lineplot(data=selected_data, x='date', y='adjusted_close', ax=ax1)
+plt.xticks(rotation=45, ha='right', fontsize=8) # Rotate labels by 45 degrees
+plt.tight_layout()
+ax1.set_title(f'{selected_ticker} Price Over Time')
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Price')
+st.pyplot(fig1)
+
+#visualization of volume of trades Over Time
+st.subheader(f"Volume of Trades Over Time for {selected_ticker}")
+fig2, ax2 = plt.subplots(figsize=(10, 6))
+sns.lineplot(data=selected_data, x='date', y='volume', ax=ax2)
+#rotate labels by 45 degrees
+plt.xticks(rotation=45, ha='right', fontsize=8)
+plt.tight_layout()
+ax2.set_title(f'{selected_ticker} Volume of Trades Over Time')
+ax2.set_xlabel('Date')
+ax2.set_ylabel('Volume')
+st.pyplot(fig2)
