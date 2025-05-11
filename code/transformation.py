b562ad58a90b30cac3c4ece6ae30fb82f46bd2f5
zoeymccarthy <zpmccart@syr.edu>
2025-05-10 22:18:30 -0400

    filter out the treasury bond rates and save it to a new csv

--- code/transformation.py
+++ code/transformation.py
@@ -8,8 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #uses the clean_stock_data function from tool_functions.py to clean the data and save it to a new csv file
-
-#aapl data
+'''#aapl data
 tf.clean_stock_data("AAPL")
 
 #jpm data
@@ -20,3 +19,17 @@ tf.clean_stock_data("SPY")
 
 #xlre data
 tf.clean_stock_data("XLRE")
+'''
+
+#gets all the rows in fed data that are after feb 2020 and contain "treasury_bonds" as the value under security_desc
+
+#read the CSV file into a DataFrame
+df = pd.read_csv("cache/fed_data.csv") 
+
+#filter rows where 'security_desc' contains 'Treasury Bond' (case-insensitive)
+df_filtered = df[df['security_desc'].str.contains('Treasury Bonds', case=False, na=False)]
+
+#save the filtered DataFrame to a new CSV file
+df_filtered.to_csv('cache/bond_rates.csv', index=True)
+
+print("Filtered data saved to 'bond_rates.csv'.")
