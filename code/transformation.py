02bfdce639e16275f3b26e01345ce99b2772b931
zoeymccarthy <zpmccart@syr.edu>
2025-05-10 22:35:25 -0400

    took the index off of bond_rates.csv

--- code/transformation.py
+++ code/transformation.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #uses the clean_stock_data function from tool_functions.py to clean the data and save it to a new csv file
+'''
 #aapl data
 tf.clean_stock_data("AAPL")
 
@@ -19,7 +20,7 @@ tf.clean_stock_data("SPY")
 
 #xlre data
 tf.clean_stock_data("XLRE")
-
+'''
 
 #gets all the rows in fed data that are after feb 2020 and contain "treasury_bonds" as the value under security_desc
 
@@ -30,7 +31,7 @@ df = pd.read_csv("cache/fed_data.csv")
 df_filtered = df[df['security_desc'].str.contains('Treasury Bonds', case=False, na=False)]
 
 #save the filtered DataFrame to a new CSV file
-df_filtered.to_csv('cache/bond_rates.csv', index=True)
+df_filtered.to_csv('cache/bond_rates.csv', index=False)
 
 print("Filtered data saved to 'bond_rates.csv'.")
 
